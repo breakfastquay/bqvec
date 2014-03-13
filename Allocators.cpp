4b7b0d67fa42dc25b377846f911a79f1ff26ce99
Chris Cannam <cannam@all-day-breakfast.com>
2014-03-13 15:15:16 +0000

    Start rearranging in order to split out bqvec and bqfft

---
+++ Allocators.cpp
@@ -0,0 +1,49 @@
+/* -*- c-basic-offset: 4 indent-tabs-mode: nil -*-  vi:set ts=8 sts=4 sw=4: */
+/* Copyright Chris Cannam - All Rights Reserved */
+
+#include "Allocators.h"
+
+#ifdef HAVE_IPP
+#include <ipps.h>
+#endif
+
+#include <iostream>
+using std::cerr;
+using std::endl;
+
+namespace BreakfastQuay {
+
+#ifdef HAVE_IPP
+
+template <>
+float *allocate(size_t count)
+{
+    float *ptr = ippsMalloc_32f(count);
+    if (!ptr) throw (std::bad_alloc());
+    return ptr;
+}
+
+template <>
+double *allocate(size_t count)
+{
+    double *ptr = ippsMalloc_64f(count);
+    if (!ptr) throw (std::bad_alloc());
+    return ptr;
+}
+
+template <>
+void deallocate(float *ptr)
+{
+    if (ptr) ippsFree((void *)ptr);
+}
+
+template <>
+void deallocate(double *ptr)
+{
+    if (ptr) ippsFree((void *)ptr);
+}
+
+#endif
+
+}
+
---
+++ ComplexTypes.h
@@ -0,0 +1,20 @@
+/* -*- c-basic-offset: 4 indent-tabs-mode: nil -*-  vi:set ts=8 sts=4 sw=4: */
+/* Copyright Chris Cannam - All Rights Reserved */
+
+#ifndef BQ_COMPLEX_TYPES_H
+#define BQ_COMPLEX_TYPES_H
+
+namespace BreakfastQuay {
+
+#ifndef NO_COMPLEX_TYPES
+// Convertible with other complex types that store re+im consecutively
+typedef double bq_complex_element_t;
+typedef struct {
+    bq_complex_element_t re;
+    bq_complex_element_t im;
+} bq_complex_t;
+#endif
+
+}
+
+#endif
