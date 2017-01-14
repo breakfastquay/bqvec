70cfd95fb36dacb1b03ec6e5d1e0e40e329a950a
Chris Cannam <cannam@all-day-breakfast.com>
2017-01-14 19:45:33 +0000

    Old version of valgrind on Travis doesn't support this flag

--- build/run-platform-tests.sh
+++ build/run-platform-tests.sh
@@ -75,7 +75,7 @@ for mf in Makefile build/Makefile.$platformtag build/Makefile.$platformtag.* ; d
     if [ "$have_valgrind" = "yes" ]; then
 	for t in test-* ; do
 	    if [ -x "$t" ]; then
-		run "no leaks are possible" valgrind --leak-check=full --show-leak-kinds=all ./"$t"
+		run "no leaks are possible" valgrind --leak-check=full ./"$t"
 	    fi
 	done
     fi
