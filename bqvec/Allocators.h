fb01311a934d041af433503bcb5322b939109083
Chris Cannam <cannam@all-day-breakfast.com>
2018-11-12 15:37:56 +0000

    No, that was a mistake. Caller can always choose not to reallocate - but can't choose to reallocate if we're refusing to do it

--- bqvec/Allocators.h
+++ bqvec/Allocators.h
@@ -243,9 +243,6 @@ void deallocate(double *);
 template <typename T>
 T *reallocate(T *ptr, size_t oldcount, size_t count)
 {
-    if ((count < oldcount) && ptr) {
-        return ptr;
-    }
     T *newptr = allocate<T>(count);
     if (oldcount && ptr) {
         size_t tocopy = oldcount;
